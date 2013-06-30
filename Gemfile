a98908ccc5b02669417fab85d611cf8228b949b4
Alter Lagos <alter.strife@gmail.com>
2013-06-29 23:50:18 -0400

    probando conexión http y estrapeo

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in raspudin.gemspec
-gemspec
+gem 'nokogiri'
+gem 'rest-client'
--- lib/raspudin.rb
+++ lib/raspudin.rb
@@ -1,5 +1,17 @@
 require "raspudin/version"
+require 'rest_client'
+require 'nokogiri'
 
 module Raspudin
-  # Your code goes here...
+
+	def self.start
+		# RestClient.proxy = 'http://115.85.73.92:8080'
+	  resp = RestClient.get 'http://whatismyipaddress.com'
+	  doc = Nokogiri::HTML(resp.body)
+	  p '-----MI IP -------------------'
+	  doc.xpath('//*[@id="wrap"]/div/table/tr/td[2]/div').each do |link|
+	  	p link.xpath('//h2/span').children[2].content
+		end
+
+	end
 end
