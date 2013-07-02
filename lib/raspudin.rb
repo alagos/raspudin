2c01e26db8a589c015cda5ee22001a14a2dc1d39
Alter Lagos <alter.strife@gmail.com>
2013-07-02 02:26:55 -0400

    Ahora almacena info escrapeada en bd. además administra lista de prxies directamente de la bd

--- lib/raspudin.rb
+++ lib/raspudin.rb
@@ -1,36 +1,36 @@
-require 'rest_client'
 require 'nokogiri'
+require 'rest_client'
 require 'settingslogic'
 require 'raspudin/dbconn'
-require 'raspudin/proxymanager'
 require 'raspudin/settings'
 require 'raspudin/version'
 
 module Raspudin
 
-	def self.start
-    pm = ProxyManager.new(Settings.proxies)
+  def self.start
     db = DbConn.new
+    db.insert_new_proxies(Settings.proxies)
     max = Settings.scrap_param_max_value
     min = db.last_param_value + 1
 
     (min..max).each do |num|
-      proxy = pm.random_proxy.address
-      p "---PROXY: #{proxy}"
-      RestClient.proxy = proxy
+      proxy = db.random_proxy
+      p "-PROXY: #{proxy[:url]}"
+      RestClient.proxy = proxy[:url]
       url = Settings.scrap_url + '?' + Settings.scrap_param + '=' + num.to_s
       begin
-        p "url: #{url}"
+        p "---URL: #{url}"
         resp = RestClient.get(url)
         doc = Nokogiri::HTML(resp.body)
+        db.ping_used_proxy(proxy)
         Settings.scrap_xpaths.each do |xpath|
           scrap = doc.xpath(xpath)
-          db.record_data(scrap.last.content, proxy, num) if scrap.any?
+          db.record_data(scrap.last.content, proxy[:url], num) if scrap.any?
         end
       rescue Exception => e
         p "excepcion: #{e.message}\n#{e.backtrace.join("\n\t")}"
-        db.record_error(proxy, num, "#{e.message}\n#{e.backtrace.join("\n\t")}")
+        db.record_error(proxy[:url], num, e.message)
       end
     end
-	end
+  end
 end
--- lib/raspudin/dbconn.rb
+++ lib/raspudin/dbconn.rb
@@ -1,14 +1,15 @@
+require 'rest_client'
 require 'sequel'
+require 'settingslogic'
 
 class DbConn
   
   def initialize(dbname = 'raspudin.sqlite3')
-    # Open a database
-    # @db = SQLite3::Database.new File.join(File.dirname(__FILE__), '../../db/' + dbname)
     @db = Sequel.connect('sqlite://' + File.join(File.dirname(__FILE__), '../../db/' + dbname))
     init_tables
   end
 
+  #### OTHERS ####
   def last_param_value
     param = @db[:others][key: 'last_param_value']
     param.nil? ? 0 : param[:value].to_i
@@ -19,6 +20,7 @@ class DbConn
     @db[:others].insert(key: 'last_param_value', value: value) if rec.update(value: value) != 1
   end
 
+  #### RECORDS ####
   def record_data(data,ip,seq)
     @db[:records].insert(data: data, ip: ip, seq: seq)
     set_last_param_value(seq)
@@ -29,21 +31,82 @@ class DbConn
     set_last_param_value(seq)
   end
 
+  #### PROXIES ####
+  def all_proxies
+    @db[:proxies].all
+  end
+
+  def all_alive_proxies
+    @db[:proxies][alive: true]
+  end
+
+  def insert_new_proxies(tmp)
+    diff = tmp - all_proxies.map { |e| e[:url] }
+    diff.each do |prx|
+      @db[:proxies].insert(url: prx)
+    end if diff.any?
+  end
+
+  def ping_used_proxy(proxy)
+    prx = @db[:proxies].where(id: proxy[:id])
+    prx.update(last_used: Time.now, count_times_used: (prx.first[:count_times_used] || 0 ) + 1)
+  end
+
+  def random_proxy
+    proxy = nil
+    proxy = random_alive_proxy while proxy.nil? || !is_alive?(proxy)
+    proxy
+  end
+
   private
 
-  def init_tables
-    @db.create_table? :records do
-      primary_key :id
-      String :data, text: true
-      String :ip
-      Fixnum :seq
-      String :error, text: true
+    def random_alive_proxy
+      @db["SELECT * FROM proxies WHERE alive = 't' ORDER BY RANDOM() LIMIT 1;"].first
     end
-    
-    @db.create_table? :others do
-      primary_key :id
-      String :key
-      String :value
+
+    def is_alive?(proxy)
+      if !proxy.nil? && proxy[:alive]
+        RestClient.proxy = proxy[:url]
+        begin
+          body = RestClient.get(Settings.ip_url).body
+          alive = proxy[:url].include? body
+          set_dead(proxy, body) unless alive
+          alive
+        rescue Exception => e
+          puts "error para #{proxy[:url]}: #{e.message}\n#{e.backtrace.join("\n\t")}"
+          set_dead(proxy, e.message)
+          false
+        end
+      end
     end
-  end
+
+    def set_dead(proxy, error = nil)
+      @db[:proxies].where(id: proxy[:id]).update(alive: false, error: error, last_used: Time.now)
+    end
+
+    def init_tables
+      @db.create_table? :records do
+        primary_key :id
+        String :data, text: true
+        String :ip
+        Fixnum :seq
+        String :error, text: true
+      end
+      
+      @db.create_table? :others do
+        primary_key :id
+        String :key
+        String :value
+      end
+
+      @db.create_table? :proxies do
+        primary_key :id
+        String :url
+        DateTime :last_used
+        Fixnum :count_times_used
+        TrueClass :alive, default: true
+        String :error, text: true
+      end
+    end
+
 end
